1e0b8d3bce668c29b972e09ce403e0bfeae28c31
RAVI SHARMA <ravisharmacs09@gmail.com>
2024-01-19 14:28:10 +0530

    created basic project structure and files and folders with navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// import common components
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+
+// import pages
+import Home from "./pages/Home/home";
+import About from "./pages/About/about";
+import Projects from "./pages/Projects/projects";
+import Resume from "./pages/Resume/resume";
+
+// import "./App.css";
+import "./style.css";
 
 function App() {
+  const [load, updateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      updateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+  return <>
+  <h5 style={{color: 'white'}}>Welcome to portfolio page</h5>
+  </>;
+};
+
+export default Home;
